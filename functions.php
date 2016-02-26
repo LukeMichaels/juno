6f470fe42697823f6e48f3642e7db299442db24d
Luke Michaels <lmichaels@gmail.com>
2016-02-26 15:02:03 -0800

    enquing main.css for now
    will switch back to main.min.css when I get css-nano (or an alternative) working

--- functions.php
+++ functions.php
@@ -98,7 +98,7 @@ function juno_conditional_scripts() {
 
 // Load styles
 function juno_styles() {
-  wp_register_style('main', get_template_directory_uri() . '/assets/css/main.min.css', array(), '1.0', 'all');
+  wp_register_style('main', get_template_directory_uri() . '/assets/css/main.css', array(), '1.0', 'all');
   wp_enqueue_style('main'); // Enqueue it!
 }
 
